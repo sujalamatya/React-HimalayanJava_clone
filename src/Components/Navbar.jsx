01566595af99720692f2542d9a1ecb2dfef12204
sujal amatya <sujalcr10@gmail.com>
2024-10-26 16:58:29 +0545

    feat(routing): added react-router-dom

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,39 @@
 import Logo from '../assets/java-newlogo.png'
 import React from 'react'
+import { useState, useEffect } from "react";
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 export default function Navbar() {
+    const [showNavbar, setShowNavbar] = useState(true);
+    const [lastScrollY, setLastScrollY] = useState(0);
+    const [topNavbar, setTopNavbar] = useState(true);
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+  
+      if (currentScrollY < 100) {
+        setTopNavbar(true);
+      } else {
+        setTopNavbar(false);
+      }
+      if (currentScrollY > lastScrollY && currentScrollY > 100) {
+        setShowNavbar(false);
+      } else {
+        setShowNavbar(true);
+      }
+  
+      setLastScrollY(currentScrollY);
+    };
+  
+    useEffect(() => {
+      window.addEventListener("scroll", handleScroll);
+  
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }, [lastScrollY]);
     return (
-        <nav className="font-sans fixed w-full top-0 z-50 p-1 px-8 bg-transparent overflow-auto border-b border-solid border-gray-300">
+        <BrowserRouter>
+        <nav className={`font-sans fixed w-full top-0 z-50 p-1 px-8 bg-transparent overflow-auto border-b border-solid border-gray-300 transition-all duration-200 ease-in-out ${
+                showNavbar ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-full'}`}>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
         <ul className="flex justify-around items-center list-none mt-4 p-0 font-medium">
             {/* Logo Item */}
@@ -16,7 +47,7 @@ export default function Navbar() {
             
             {/* Navigation Items */}
             <li className=" py-2 cursor-pointer relative group">
-                    <span className="relative text-gray-800 hover:text-black">Home</span>
+                    <span className="relative text-gray-800 hover:text-black"><Link to="/">Home</Link></span>
                     <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-black transition-all duration-300 ease-in-out group-hover:w-full"></span>
             </li>
             <li className=" py-2 cursor-pointer relative group">
@@ -51,6 +82,6 @@ export default function Navbar() {
             </li>
         </ul>
     </nav>
-        
+        </BrowserRouter>  
     )
 }
--- src/Components/Roasts.jsx
+++ src/Components/Roasts.jsx
@@ -9,26 +9,26 @@ export default function () {
     const ref = useRef();
     const isInView = useInView(ref);
   return (
-    <div className='w-screen h-[19em] bg-green-950 items-end'>
-        <div className='flex justify-around'>
-            <motion.div ref={ref} initial={{opacity:0,y:+30}} animate={isInView && {opacity:1,y:0}} transition={{ duration: 1 }}>
+    <div className='w-screen h-[14em] bg-green-950 items-end mt-[8em]'>
+        <div className='flex justify-around '>
+            <motion.div ref={ref} initial={{opacity:0,y:+30}} animate={isInView && {opacity:1,y:-80}} transition={{ duration: 0.5 }}>
                 <img src={coffee1} alt="coffieOne" />
             </motion.div>
-            <motion.div ref={ref} initial={{opacity:0,y:+30}} animate={isInView && {opacity:1,y:0}} transition={{ duration: 1.4 }}>
+            <motion.div ref={ref} initial={{opacity:0,y:+30}} animate={isInView && {opacity:1,y:-80}} transition={{ duration: 0.7 }}>
                 <img src={coffee2} alt="coffieTwo" />
             </motion.div>
-            <motion.div ref={ref} initial={{opacity:0,y:+30}} animate={isInView && {opacity:1,y:0}} transition={{ duration: 1.8 }}>
+            <motion.div ref={ref} initial={{opacity:0,y:+30}} animate={isInView && {opacity:1,y:-80}} transition={{ duration: 0.9 }}>
                 <img src={coffee3} alt="coffieThree" />
             </motion.div>
-            <motion.div ref={ref} initial={{opacity:0,y:+30}} animate={isInView && {opacity:1,y:0}} transition={{ duration: 2 }}>
+            <motion.div ref={ref} initial={{opacity:0,y:+30}} animate={isInView && {opacity:1,y:-80}} transition={{ duration: 1 }}>
                 <img src={coffee4} alt="coffieFour" />
             </motion.div>
         </div>
         <div className='flex justify-around text-white mt-6'>
-            <motion.div ref={ref} initial={{opacity:0,y:+30}} animate={isInView && {opacity:1,y:0}} transition={{ duration: 1.4 }}><h1>THAMEL ROAST</h1></motion.div>
-            <motion.div ref={ref} initial={{opacity:0,y:+30}} animate={isInView && {opacity:1,y:0}} transition={{ duration: 1 }}><h1>EVEREST ROAST</h1></motion.div>
-            <motion.div ref={ref} initial={{opacity:0,y:+30}} animate={isInView && {opacity:1,y:0}} transition={{ duration: 1.8 }}><h1>ESPRESSO ROAST</h1></motion.div>
-            <motion.div ref={ref} initial={{opacity:0,y:+30}} animate={isInView && {opacity:1,y:0}} transition={{ duration: 2 }}><h1>MEDIUM ROAST</h1></motion.div>
+            <motion.div ref={ref} initial={{opacity:0,y:+30}} animate={isInView && {opacity:1,y:-80}} transition={{ duration: 0.5 }}><h1>THAMEL ROAST</h1></motion.div>
+            <motion.div ref={ref} initial={{opacity:0,y:+30}} animate={isInView && {opacity:1,y:-80}} transition={{ duration: 0.7 }}><h1>EVEREST ROAST</h1></motion.div>
+            <motion.div ref={ref} initial={{opacity:0,y:+30}} animate={isInView && {opacity:1,y:-80}} transition={{ duration: 0.9 }}><h1>ESPRESSO ROAST</h1></motion.div>
+            <motion.div ref={ref} initial={{opacity:0,y:+30}} animate={isInView && {opacity:1,y:-80}} transition={{ duration: 1 }}><h1>MEDIUM ROAST</h1></motion.div>
         </div>
     </div>
   )
